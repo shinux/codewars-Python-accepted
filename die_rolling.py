e7476912b6f830635b6425dfa9e6514fa2d80171
Sinux <wshmelo@gmail.com>
2015-09-16 16:29:49 +0800

    Die Rolling

---
+++ die_rolling.py
@@ -0,0 +1,14 @@
+"""
+Description:
+
+    Hello! Today your task is to build a basic die feature, where you will get a range in the form (min, max) - both included - and return a random number in the inclusive range.
+
+    Props if you don't use your language's random library!
+
+    dice(2, 7) # returns a value that can be 2, 3, 4, 5, 6, 7
+"""
+
+import random
+
+def dice(minimum, maximum):
+    return random.randint(minimum, maximum)
